9bb665ef3a32d1252a71c57f0f45af65533f0793
Nathaniel Felsen <n@medium.com>
2016-07-11 23:58:31 -0700

    test

--- test/functional/helloworld.js
+++ test/functional/helloworld.js
@@ -11,17 +11,6 @@ describe('main page', function() {
     this.browser.visit('/', done);
   });
 
-it('should give more info', function(done) {
-    this.timeout(30000);
-
-    setTimeout(function () {
-      run.anotherMethod(global, function(err, result) {
-        expect(result).to.be.an('object');
-        done();
-      });
-    }, 30000);
- });
-
   it('should say hello world', function() {
     assert.ok(this.browser.success);
     assert.equal(this.browser.text(), "Hello World");
